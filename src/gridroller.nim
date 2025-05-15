304616fe43e2eb9b6113518748a437171bdb172a
Chris Stehno <chris.stehno@netscout.com>
2025-05-15 08:05:50 -0500

    listed the cells below

--- src/gridroller.nim
+++ src/gridroller.nim
@@ -1,10 +1,12 @@
-import random, strutils
+import random, strutils, strformat
 
 const
   Rows = 23
   Cols = 28
 
 randomize()
+
+var ones: seq[(int,int)] = @[] # col, row
 
 # Print column headers
 stdout.write("     ")  # space for row labels
@@ -25,6 +27,27 @@ for row in 0..<Rows:
     let roll = rand(1..6)
     if roll == 1:
       stdout.write(" X ")
+      ones.add((col, row))
     else:
       stdout.write("   ")
   echo "|"
+
+# Print horizontal divider
+stdout.write("    +")
+for _ in 0..<Cols:
+  stdout.write("---")
+echo "+"
+
+stdout.write("\n##### Cells #####\n")
+let cols = 10
+let rows = int((ones.len + cols - 1) / cols)
+for row in 0..rows:
+  for col in 0..cols:
+    let index = row + col * rows
+    if index < ones.len:
+      let (c,r) = ones[index]
+      # stdout.write "(", c, ", ", r, ") \t"
+      stdout.write fmt"({c:>2},{r:>2})  "
+    else: 
+      stdout.write " ".repeat(10)
+  echo ""
